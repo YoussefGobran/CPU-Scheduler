20c101ade0ddfd9b5f01fd576b4a3bbb5e3b3d7d
Youssef <youssef.saad.gobran@gmail.com>
2024-04-14 18:31:38 +0200

    edits

--- src/main/java/com/example/cpu_scheduler/HelloApplication.java
+++ src/main/java/com/example/cpu_scheduler/HelloApplication.java
@@ -4,9 +4,12 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -15,8 +18,18 @@ import java.io.IOException;
 public class HelloApplication extends Application implements EventHandler<ActionEvent> {
     @Override
     public void start(Stage stage) throws IOException {
+        Text t = new Text (10, 20, "choose scheduler");
         Button ok = new Button("ok");
-        Scene scene = new Scene(ok, 320, 240);
+        ok.setOnAction((e)->{
+            System.out.println("hello");
+        });
+        GridPane pane = new GridPane();
+        pane.setHgap(8); // Set horizontal gap
+        pane.setVgap(32); // Set vertical gap
+        pane.setPadding(new Insets(48, 48, 48, 48));
+        pane.add(ok,2,2);
+        pane.add(t,1,1);
+        Scene scene = new Scene(pane);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -27,4 +40,8 @@ public class HelloApplication extends Application implements EventHandler<Action
     }
 
 
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
+    }
 }
